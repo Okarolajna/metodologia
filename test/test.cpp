658376d7e0733e2f5dc6121a45b3c00a4ba9b359
Karolina Jablonska <k.jablonska1985@gmail.com>
2023-03-27 18:14:06 +0200

    Update files

---
+++ test/test.cpp
@@ -0,0 +1,24 @@
+#include "../src/code.hpp"
+
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+int main(){
+
+if(multiple(1) == 0){
+    cout << "The number must be bigger than 0!" << endl;
+    return 0;
+}
+
+if(multiple(-1) < 0){
+    cout << "The number must be bigger than 0!" << endl;
+    return 0;
+}
+
+
+cout << "OK" << endl;
+
+
+    return 0;
+}
